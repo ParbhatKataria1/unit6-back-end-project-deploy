787a5e486e149d76d07d44219bc53b28938bf2f0
ParbhatKataria1 <varshagagangandhi@gmail.com>
2023-03-30 14:34:58 +0530

    done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { men } = require('./routes/men.routes');
 const { order } = require('./routes/order.routes');
 const { women } = require('./routes/women.routes');
 require('dotenv').config();
-var cors = require('cors')
+const cors = require('cors')
 
 
 app.use(cors())
@@ -36,52 +36,52 @@ app.listen(process.env.port, async(req, res)=>{
 //     images:Array,
 //     color:Array,
 //     type:String
-function con(){
-   let item =  {
-        "title":'Branded Elastic 3 Pack Organic Cotton Bralettes',
-        "price":1290,
-        "size":['xs','s','m','l' ],
-        "color":['white', 'blue', 'red','yellow'],
-        "type":'men',
-        "images":['https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3234.jpg?v=1671078617&width=600',
-                'https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3287.jpg?v=1677136742&width=600',
-                'https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3272.jpg?v=1677136742&width=600'
-                ]
+// function con(){
+//    let item =  {
+//         "title":'Branded Elastic 3 Pack Organic Cotton Bralettes',
+//         "price":1290,
+//         "size":['xs','s','m','l' ],
+//         "color":['white', 'blue', 'red','yellow'],
+//         "type":'men',
+//         "images":['https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3234.jpg?v=1671078617&width=600',
+//                 'https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3287.jpg?v=1677136742&width=600',
+//                 'https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3272.jpg?v=1677136742&width=600'
+//                 ]
     
-    }
-    let cart = {
-        "title":"Branded Elastic 3 Pack Organic Cotton Bralettes",
-        "price":1290,
-        "brand":"ko",
-        "size":"xs",
-        "color":"white",
-        "images":["https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3234.jpg?v=1671078617&width=600",
-                "https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3287.jpg?v=1677136742&width=600",
-                "https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3272.jpg?v=1677136742&width=600"
-                ],
-        "type":"men"
-    }
-    let order = {
-        "title":"Branded Elastic 3 Pack Organic Cotton Bralettes",
-        "price":1290,
-        "brand":"ko",
-        "size":"xs",
-        "color":"white",
-        "images":["https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3234.jpg?v=1671078617&width=600",
-                "https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3287.jpg?v=1677136742&width=600",
-                "https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3272.jpg?v=1677136742&width=600"
-                ],
-        "type":"men",
-        "qty":3,
-        "pincode":14555,
-        "city":"guru",
-        "address":"#316 gurunanak",
-        "state":"punjab",
-        "status":false,
-        "date":'12th feb 12:30'
-    }
+//     }
+//     let cart = {
+//         "title":"Branded Elastic 3 Pack Organic Cotton Bralettes",
+//         "price":1290,
+//         "brand":"ko",
+//         "size":"xs",
+//         "color":"white",
+//         "images":["https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3234.jpg?v=1671078617&width=600",
+//                 "https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3287.jpg?v=1677136742&width=600",
+//                 "https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3272.jpg?v=1677136742&width=600"
+//                 ],
+//         "type":"men"
+//     }
+//     let order = {
+//         "title":"Branded Elastic 3 Pack Organic Cotton Bralettes",
+//         "price":1290,
+//         "brand":"ko",
+//         "size":"xs",
+//         "color":"white",
+//         "images":["https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3234.jpg?v=1671078617&width=600",
+//                 "https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3287.jpg?v=1677136742&width=600",
+//                 "https://cdn.shopify.com/s/files/1/0677/1464/6315/products/koovs-3272.jpg?v=1677136742&width=600"
+//                 ],
+//         "type":"men",
+//         "qty":3,
+//         "pincode":14555,
+//         "city":"guru",
+//         "address":"#316 gurunanak",
+//         "state":"punjab",
+//         "status":false,
+//         "date":'12th feb 12:30'
+//     }
 
-}
+// }
     // title : String,
     // price : String,
     // brand : String,
