7f14f92036ba1e2c3370d4bb42eabeaa7c9b5101
ParbhatKataria <parbhatkataria628@gmail.com>
2023-06-02 18:38:35 +0530

    done

--- controller/allorder.controller.js
+++ controller/allorder.controller.js
@@ -1,9 +1,11 @@
 const { OrderModel } = require("../model/order.model");
 
 const addPost = async (req, res) => {
+  // console.log(req.body)
   try {
     let item = new OrderModel(req.body);
     await item.save();
+    console.log(item)
     res.status(200).send({ msg: "item is stored" });
   } catch (error) {
     res.status(400).send({ msg: "item is not able to store in the database" });
@@ -11,12 +13,18 @@ const addPost = async (req, res) => {
 };
 
 const getPosts = async (req, res) => {
-  let { page, limit } = req.query;
+  let { page, limit, type } = req.query;
   limit = limit == undefined ? 8 : limit;
   try {
     // let { userId } = req.body;
     let obj = {};
+    if(type){
+      obj.type = type;
+    }
+    console.log(type, obj)
     let data;
+    let length = await OrderModel.find(obj);
+    length = length.length;
     if (page) {
       data = await OrderModel.find(obj)
         .skip((page - 1) * limit)
@@ -24,7 +32,7 @@ const getPosts = async (req, res) => {
     } else {
       data = await OrderModel.find(obj).limit(limit);
     }
-    res.status(200).send(data);
+    res.status(200).send({data, length});
   } catch (error) {
     res.status(400).send({ msg: "not able to get the data" });
   }
